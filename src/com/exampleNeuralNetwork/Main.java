021136edad77cb34fd9120ed2e7dfd6e5cc190e9
David <david_pogosian@outlook.com>
2024-01-16 17:54:45 -0500

    Initial commit.

---
+++ src/com/exampleNeuralNetwork/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Read input image.
+
+        // Train neural network.
+
+        // Present image to neural network and get output.
+    }
+}
